8cf5a7f6b87a817e8eb70cd5dd292deedf8609af
Aar Lazuardi <alazuardi7@gmail.com>
2025-05-16 09:37:10 +0700

    Update API base URL dan tambahkan .env.local

--- next/lib/api.js
+++ next/lib/api.js
@@ -1,4 +1,5 @@
-export const API_BASE = "http://127.0.0.1:5000";
+export const API_BASE =
+  process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5000";
 
 /**
  * API utility for Shopee Growth Prediction
